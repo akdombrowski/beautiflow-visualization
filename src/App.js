61b09c0e1e05b313fea6aa50fe606a1bd51b64c0
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-03-01 19:16:00 -0600

    remove/update old refs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Container from "react-bootstrap/Container";
 import Stack from "react-bootstrap/Stack";
 import Row from "react-bootstrap/Row";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import reportWebVitals from "./reportWebVitals.js";
+import reportWebVitals from "./report-web-vitals.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
