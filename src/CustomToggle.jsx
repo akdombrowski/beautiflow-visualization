7693dc543b3edceaa64a757f42bb196bca97f98f
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-03-01 18:26:27 -0600

    xo setup

--- src/CustomToggle.jsx
+++ src/CustomToggle.jsx
@@ -1,20 +1,20 @@
-import Accordion from "react-bootstrap/Accordion";
-import Button from "react-bootstrap/Button";
-import { useAccordionButton } from "react-bootstrap/AccordionButton";
-import Card from "react-bootstrap/Card";
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import {useAccordionButton} from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
 
-const CustomToggle = ({ children, eventKey, setAccordionCollapsedState }) => {
-  const decoratedOnClick = useAccordionButton(eventKey, () => {
-    setAccordionCollapsedState(eventKey);
-  });
+function CustomToggle({children, eventKey, setAccordionCollapsedState}) {
+	const decoratedOnClick = useAccordionButton(eventKey, () => {
+		setAccordionCollapsedState(eventKey);
+	});
 
-  return (
-    <div className="d-grid h-100 w-100">
-      <Button variant="secondary" style={{}} onClick={decoratedOnClick}>
-        {children}
-      </Button>
-    </div>
-  );
-};
+	return (
+		<div className='d-grid h-100 w-100'>
+			<Button variant='secondary' style={{}} onClick={decoratedOnClick}>
+				{children}
+  </Button>
+  </div>
+	);
+}
 
 export default CustomToggle;
