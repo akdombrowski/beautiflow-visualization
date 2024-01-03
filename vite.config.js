68a3f674519c000bdff2660409e9a1edeb638697
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-01-02 22:24:08 -0600

    use port 3000 only in dev mode

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
-  resolve: {
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  let config = {};
+  config.plugins = [react()];
+  config.resolve = {
     alias: [
       {
-        // this is required for the SCSS modules
+        // this is required for the SCSS modules as it allows a leading ~
         find: /^~(.*)$/,
         replacement: "$1",
       },
     ],
-  },
+  };
+  
+  if (command === "serve" || mode === "development") {
+    config.server = { port: 3000 };
+  }
+
+  return config;
 });
